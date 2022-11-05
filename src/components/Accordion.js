1d544865242aab8b676c259da43737ec3a394d10
karpuninaleksandr <aleksandrkarpunin0889@gmail.com>
2022-11-05 14:24:49 +0300

    main page v1.1

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -2,27 +2,30 @@ export default function Accordion() {
     return (
         <div className = "accordion">
             <div className = "form">
-                <p>Фамилия: 
-                <input readOnly type = "text"  name = "surname"></input></p>
-                <p>Имя: 
-                <input readOnly type = "text"  name = "name"></input></p>
-                <p>Отчество: 
-                <input readOnly type = "text"  name = "lastname"></input></p>
-                <p><strong>Причина вызова</strong></p>
+                <p>Фамилия: <input readOnly type = "text" placeholder = "Иванов" name = "surname"></input></p>
+                <p>Имя: <input readOnly type = "text" placeholder = "Иван" name = "name"></input></p>
+                <p>Отчество: <input readOnly type = "text" placeholder = "Иванович" name = "lastname"></input></p>
+                <p>Причина вызова</p>
                 <select className = "selectList">
-                    <option disabled>Выберите из списка</option>
+                    <option disabled selected>Выберите из списка</option>
                     <option>Пожар</option>
                     <option>Преступление</option> 
                     <option>Наводнение</option> 
                     <option>Утечка газа</option> 
                     <option>Другое</option> 
-                </select><br></br>
-                <textarea className = "textHelp" placeholder = "Опишите по возможности ситуацию"></textarea>
+                </select>
+                <br></br>
+                <p>Нужная информация</p>
+                <textarea className = "textHelp" placeholder = "Опишите, по возможности, ситуацию"></textarea>
                 <p>Есть ли пострадавшие?</p>
-                <input type="checkbox" value="Yes" checked="checked" /> Да
+                <input type="checkbox" value="Yes" /> Да
+                <br></br>
                 <input type="checkbox" value="No" /> Нет
-                <input type="checkbox" value="X3" /> Не знаю
-                <button className = 'challenge'>Вызов</button>
+                <br></br>
+                <input type="checkbox" value="NoInfo" /> Не знаю
+                <br></br>
+                <br></br>
+                <button className = 'call'>Вызов</button>
             </div>
         </div>
     )
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <div className = "header">
-            <h1>Служба защиты Онлайн</h1>
+            <h2>Служба защиты Онлайн</h2>
             <button className = 'button_entrance'>Вход</button>
         </div>
     )
