342e3768782fead54c5972b271f87052010da45f
karpuninaleksandr <aleksandrkarpunin0889@gmail.com>
2022-11-05 18:40:46 +0300

    main page v1.3

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -2,21 +2,35 @@ export default function Accordion() {
     return (
         <div className = "accordion">
             <div className = "form">
-                <p>Фамилия: <input readOnly type = "text" placeholder = "Иванов" name = "surname"></input></p>
-                <p>Имя: <input readOnly type = "text" placeholder = "Иван" name = "name"></input></p>
-                <p>Отчество: <input readOnly type = "text" placeholder = "Иванович" name = "lastname"></input></p>
-                <p>Причина вызова</p>
-                <select className = "selectList">
-                    <option disabled selected>Выберите из списка</option>
-                    <option>Пожар</option>
-                    <option>Преступление</option> 
-                    <option>Наводнение</option> 
-                    <option>Утечка газа</option> 
-                    <option>Другое</option> 
-                </select>
+                <div class="input_info">
+                    <p>Фамилия:</p>
+                    <input type = "text" placeholder = "Иванов" name = "surname"></input>
+                </div>
+                <div class="input_info">
+                    <p>Имя:</p><input type = "text" placeholder = "Иван" name = "name"></input>
+                </div>
+                <div class="input_info">
+                    <p>Отчество:</p>
+                    <input type = "text" placeholder = "Иванович" name = "lastname"></input>
+                </div>
+                <div class="input_info">
+                   <p>Причина вызова</p>
+                    <br></br>
+                    <select className = "selectList">
+                        <option disabled selected>Выберите из списка</option>
+                        <option>Пожар</option>
+                        <option>Преступление</option> 
+                        <option>Наводнение</option> 
+                        <option>Утечка газа</option> 
+                        <option>Другое</option> 
+                    </select> 
+                </div>
                 <br></br>
-                <p>Нужная информация</p>
-                <textarea className = "textHelp" placeholder = "Опишите, по возможности, ситуацию"></textarea>
+                <div class="input_info">
+                    <p>Нужная информация</p>
+                    <textarea className = "textHelp" placeholder = "Опишите ситуацию"></textarea>
+                </div>
+                
                 <p>Есть ли пострадавшие?</p>
                 <input type="checkbox" value="Yes" /> Да
                 <br></br>
@@ -29,5 +43,4 @@ export default function Accordion() {
             </div>
         </div>
     )
-
 }
