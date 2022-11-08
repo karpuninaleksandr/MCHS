5b352e118dab1b81d568e88ac70528a1194b726c
Egor Roganin <eroganin@bk.ru>
2022-11-08 19:56:10 +0300

    add transaction ecorator

--- backend/call/views.py
+++ backend/call/views.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
@@ -7,6 +8,7 @@ from . import serializers
 class CreateCall(APIView):
     """Method for create call record"""
 
+    @transaction.atomic
     def post(self, request):
         serializer = serializers.CallSerializer(data=request.data)
         print(res := serializer.is_valid(), serializer.data, end='\n')
