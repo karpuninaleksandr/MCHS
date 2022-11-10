9dab32ac352d8b1f0a648b41a808f17f5a1baf7e
ViDjey <vikabu2002@mail.ru>
2022-11-10 22:46:07 +0300

    поля

--- src/components/ManagementAccordion.js
+++ src/components/ManagementAccordion.js
@@ -3,7 +3,13 @@ import ClientInput from "../inputs/InputHelper"
 export default function ManagementAccordion() {
     return (
         <div className = "management_accordion">
-            <p>вызовы</p>
+            <h3>Вызовы</h3>
+            <ul className="call">
+                <li>Пожар</li>
+                <li>Другое</li>
+                <li>Другое</li>
+                <li>Пожар</li>
+            </ul>
         </div>
     )
 }
