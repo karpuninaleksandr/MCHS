ed72b7e3aa86297ead9a7347ce9cfc33f7f3e2cd
ViDjey <vikabu2002@mail.ru>
2022-12-14 21:07:05 +0300

    minor fix

--- src/inputs/ManagementInputHelper.js
+++ src/inputs/ManagementInputHelper.js
@@ -1,5 +1,5 @@
 export default function ManagementInput(props) {
-    if (props.call === undefined) return <p>Null</p>
+    if (props.call == undefined) return <p>Null</p>
     else 
     return <div className="box_name">
             {props.call.person.surname}{props.call.person.name}{props.call.patronymic}
