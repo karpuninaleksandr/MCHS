0c338301aac8036d10943af066f0502c5168da1d
Egor <eroganin@bk.ru>
2022-12-15 09:53:23 +0300

    d

--- backend/call/views.py
+++ backend/call/views.py
@@ -4,7 +4,7 @@ from rest_framework import serializers, status
 from rest_framework.views import APIView
 from . import serializers
 from .choices import Role
-from .models import Call, Person
+from .models import Call, Person, CallToPerson
 from django.db.models.query import Prefetch
 
 
@@ -17,18 +17,30 @@ class CallView(APIView):
         return Response(status=status.HTTP_200_OK)
 
     def get(self, request):
-        queryset = Call.objects.filter(calltoperson__person__role=Role.WORKER)
+        queryset = Call.objects.exclude(calltoperson__person__role=Role.WORKER)
         serializer = serializers.CallSerializerForGetMethod(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request):
-        call_id = request.get('id')
+        call_id = request.data.get('id')
         call = Call.objects.get(id=call_id)
-        workers_id = request.get('workers')
+        workers_id = request.data.get('workers')
         workers = Person.objects.filter(id__in=workers_id)
         for worker in workers:
-            worker.call.set(call)
+            CallToPerson(person=worker, call=call).save()
+            print('sdf')
+        return Response(status=status.HTTP_200_OK)
 
+# {
+# "id": 1,
+# "workers":[]
+# }
+
+# {
+# "name":"oleg",
+# "surname":"jopa",
+# "patronymic":"hgk"
+# }
 
 class PersonView(APIView):
     def get(self, request):
