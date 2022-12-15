2c94c4f254dba267ad2447f740753e9da9b4a900
karpuninaleksandr <aleksandrkarpunin0889@gmail.com>
2022-12-15 22:13:09 +0300

    engl

--- backend/call/choices.py
+++ backend/call/choices.py
@@ -7,6 +7,6 @@ class Role(models.TextChoices):
 
 
 class Injury(models.TextChoices):
-    YES = 'Да'
-    NO = 'Нет'
-    UNKNOWN = 'Нет инфорамции'
+    YES = 'Yes'
+    NO = 'No'
+    UNKNOWN = 'Unknown'
