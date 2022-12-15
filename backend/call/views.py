6e3d5bb72e098dd2ebbc657be745d1e86bac34e2
mich <mich921@yandex.ru>
2022-12-15 20:32:05 +0300

    Полная документация

--- backend/call/views.py
+++ backend/call/views.py
@@ -12,16 +12,19 @@ class CallView(APIView):
     """Method for create call record"""
     @transaction.atomic
     def post(self, request):
+        """ Save call """
         (serializer := serializers.CallSerializerForUpdateData(data=request.data)).is_valid(raise_exception=True)
         serializer.save()
         return Response(status=status.HTTP_200_OK)
 
     def get(self, request):
+        """ Show calls """
         queryset = Call.objects.filter(calltoperson__person__role=Role.WORKER)
         serializer = serializers.CallSerializerForGetMethod(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request):
+        """ Add workers to the call """
         call_id = request.get('id')
         call = Call.objects.get(id=call_id)
         workers_id = request.get('workers')
@@ -31,20 +34,23 @@ class CallView(APIView):
 
 
 class PersonView(APIView):
-    """Method for create person record"""
+    """ Method for create person record"""
     def get(self, request):
+        """ Show workers """
         queryset = Person.objects.filter(role=Role.WORKER)
         serializer = serializers.PersonSerializerForGetMethod(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     @transaction.atomic
     def post(self, request):
+        """ Save person """
         (serializer := serializers.PersonSerializerForAddWorker(data=request.data)).is_valid(raise_exception=True)
         serializer.save()
         return Response(status=status.HTTP_200_OK)
 
 
 class CallWithPeoplesView(APIView):
+    """ Show all calls """
     def get(self, request):
         queryset = Call.objects.all()
         serializer = serializers.CallSerializerForGetMethod(queryset, many=True)
